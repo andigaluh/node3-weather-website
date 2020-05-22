11aab362a77777fda689c0d8721acfafad961660
Andi Galuh <andy13galuh@gmail.com>
2020-05-22 16:37:49 +0700

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (long, lat , callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=6971f9302702259bf058dcf2a4acac17&query=' + long + ',' + lat
+
+    request({url, json: true}, (error, { body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location!', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out, it feels like ' + body.current.feelslike + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
